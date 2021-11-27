81ae0708058dfa82e5eeefc1b707145b01e0908e
bmalkes <bmalkes@gmail.com>
2021-11-27 19:33:07 +0200

    feat:listing to enviroment port

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ connectToDataBase();
 
 app.use("/tasks", TaskRouter);
 
-app.listen(8000, () => {
-    console.log("listening on port 8000");
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
